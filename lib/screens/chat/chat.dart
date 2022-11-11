3fdd65a2ed655d5c7007e3e57e906ec21ad5dd4a
Guillermo Miranda <gdmn3000@gmail.com>
2022-11-10 22:09:11 -0300

    Agregar pantalla de chat

--- lib/screens/chat/chat.dart
+++ lib/screens/chat/chat.dart
@@ -13,7 +13,77 @@ class Chat extends StatelessWidget {
       appBar: AppBar(
         title: Text(entrenador.nombreMostrado),
       ),
-      body: Text('here'),
+      body: null,
     );
   }
+
+  // Widget buildInput() {
+  //   return Container(
+  //       child: Row(
+  //         children: <Widget>[
+  //           // Button send image
+  //           Material(
+  //             child: Container(
+  //               margin: EdgeInsets.symmetric(horizontal: 1),
+  //               child: IconButton(
+  //                 icon: Icon(Icons.image),
+  //                 onPressed: getImage,
+  //                 color: ColorConstants.primaryColor,
+  //               ),
+  //             ),
+  //             color: Colors.white,
+  //           ),
+  //           Material(
+  //             child: Container(
+  //               margin: EdgeInsets.symmetric(horizontal: 1),
+  //               child: IconButton(
+  //                 icon: Icon(Icons.face),
+  //                 onPressed: getSticker,
+  //                 color: ColorConstants.primaryColor,
+  //               ),
+  //             ),
+  //             color: Colors.white,
+  //           ),
+  //
+  //           // Edit text
+  //           Flexible(
+  //             child: Container(
+  //               child: TextField(
+  //                 onSubmitted: (value) {
+  //                   onSendMessage(textEditingController.text, TypeMessage.text);
+  //                 },
+  //                 style: TextStyle(color: ColorConstants.primaryColor, fontSize: 15),
+  //                 controller: textEditingController,
+  //                 decoration: InputDecoration.collapsed(
+  //                   hintText: 'Type your message...',
+  //                   hintStyle: TextStyle(color: ColorConstants.greyColor),
+  //                 ),
+  //                 focusNode: focusNode,
+  //                 autofocus: true,
+  //               ),
+  //             ),
+  //           ),
+  //
+  //           // Button send message
+  //           Material(
+  //             child: Container(
+  //               margin: EdgeInsets.symmetric(horizontal: 8),
+  //               child: IconButton(
+  //                 icon: Icon(Icons.send),
+  //                 onPressed: () => null,
+  //
+  //               ),
+  //             ),
+  //             color: Colors.white,
+  //           ),
+  //         ],
+  //       ),
+  //       width: double.infinity,
+  //       height: 50,
+  //       decoration: BoxDecoration(
+  //   border: Border(top: BorderSide(color: ColorConstants.greyColor2, width: 0.5)), color: Colors.white),
+  //   );
+  // }
+
+
 }
